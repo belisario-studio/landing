3ead77ceec85343ea7d7e676ac08386b7f0275a7
Emilia <66620157+emiliacb@users.noreply.github.com>
2025-11-21 03:22:31 -0300

    small fixes

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -6,7 +6,7 @@ import { usePathname } from "next/navigation"
 export default function Navigation() {
   return (
     <nav className="absolute top-0 w-full z-50 select-none">
-      <div className="max-w-7xl mx-auto px-6 py-4 flex items-center justify-between">
+      <div className="max-w-7xl mx-auto px-3 md:px-6 py-4 flex items-center justify-between">
         <Link href="/" className="text-2xl font-bold tracking-tight opacity-25 transition-colors text-muted-foreground hover:text-foreground">
           Belisario
         </Link>
