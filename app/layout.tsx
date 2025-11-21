0976727be2750525c9f7a58ea63ec8ed4c1cc3f5
Emilia <66620157+emiliacb@users.noreply.github.com>
2025-11-21 02:12:21 -0300

    remove font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,10 @@
 import type React from "react"
 import type { Metadata } from "next"
-import { Geist, Geist_Mono } from "next/font/google"
+import { Bebas_Neue } from "next/font/google"
 import { Analytics } from "@vercel/analytics/next"
 import "./globals.css"
 
-const _geist = Geist({ subsets: ["latin"] })
-const _geistMono = Geist_Mono({ subsets: ["latin"] })
+const _Bebas_Neue = Bebas_Neue({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
   title: "Belisario Studio - Game Development",
